c86ed26d72179036d828cb3fb38f8ceda210d874
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-08-18 14:18:20 +0300

    Класс для подключения к БД 2-й коммит

--- src/DBManager.py
+++ src/DBManager.py
@@ -1,5 +1,7 @@
+from decimal import Decimal
 
 import psycopg2
+
 class DBManager:
     """
     Класс для подключения к БД
@@ -13,28 +15,75 @@ class DBManager:
         """
         Метод получает список всех компаний и количество вакансий у каждой компании
         """
-        pass
+        self.cur.execute(
+            """
+            SELECT c.company_name, COUNT(v.employer_id)
+            FROM companies c
+            JOIN vacancies v USING (employer_id)
+            GROUP BY c.company_name
+            ORDER BY COUNT DESC
+            """
+        )
+        return self.cur.fetchall()
 
     def get_all_vacancies(self):
         """
         Метод получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты
         и ссылки на вакансию
         """
-        pass
+        self.cur.execute(
+            """
+            SELECT c.company_name, v.vacancy_name, v.salary_from, v.salary_to, v.salary_currency, v.alternate_url
+            FROM vacancies v
+            JOIN companies c USING (employer_id)
+            WHERE v.salary_from != 0 AND v.salary_from IS NOT NULL AND v.salary_to != 0 AND v.salary_to IS NOT NULL
+            ORDER BY v.salary_from DESC
+            """
+        )
+        return self.cur.fetchall()
 
     def get_avg_salary(self):
         """
         Метод получает среднюю зарплату по вакансиям
         """
-        pass
+        self.cur.execute(
+            """
+            SELECT AVG((salary_from + salary_to) / 2) AS avg_salary
+            FROM vacancies
+            WHERE salary_from IS NOT NULL AND salary_to IS NOT NULL
+            """
+        )
+        result = self.cur.fetchone()
+        avg_salary = Decimal(result[0])
+        formatted_avg_salary = format(avg_salary, '.2f')
+        return formatted_avg_salary
 
     def get_vacancies_with_higher_salary(self):
         """
         Метод получает список всех вакансий, у которых зарплата выше средней по всем вакансиям
         """
-        pass
+        avg_salary = self.get_avg_salary()[0][0]
 
-    def get_vacancies_with_keyword(self):
+        self.cur.execute(
+            """
+            SELECT v.vacancy_name, v.salary_from, v.salary_to, v.alternate_url
+            FROM vacancies v
+            WHERE ((v.salary_from + v.salary_to) / 2) > %s
+            """, (avg_salary,)
+        )
+        return self.cur.fetchall()
+
+    def get_vacancies_with_keyword(self, keyword=None):
         """
         Метод получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python
         """
+        keyword = f"%{keyword.lower()}%"
+
+        self.cur.execute(
+            """
+            SELECT v.vacancy_name, v.salary_from, v.salary_to, v.alternate_url
+            FROM vacancies v
+            WHERE LOWER(v.vacancy_name) LIKE %s
+            """, (keyword,)
+        )
+        return self.cur.fetchall()
