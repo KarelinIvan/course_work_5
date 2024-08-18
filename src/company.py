05c79a43da61b48dba855000f65c8a7edf05bf85
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-08-18 12:07:10 +0300

    Класс для работы с компаниями 2-й коммит

--- src/company.py
+++ src/company.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 class Company:
     """
     Класс для работы с компанией
@@ -25,3 +27,20 @@ class Company:
             f"URL адрес компании: {self.site_url}\n"
             f"Количество открытых вакансий: {self.open_vacancies}\n"
         )
+
+@classmethod
+def from_company_cls(cls, company_date: dict) -> Any:
+    """
+    Метод возвращает экземпляр класса
+    """
+    if isinstance(company_date, dict):
+        return cls(
+            company_date.get("id"),
+            company_date.get("name"),
+            company_date.get("description", "нет описания"),
+            company_date.get("site_url", "нет URL"),
+            company_date.get("open_vacancies", 0)
+        )
+    else:
+        print("Ошибка: данные компании должны быть словарем")
+        return None
