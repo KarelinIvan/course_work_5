e4c5acd100e37232dbec88425a9be1bd56a6fcf0
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-08-18 15:10:11 +0300

    Класс для подключения к БД 2-й коммит

---
+++ main.py
@@ -0,0 +1,26 @@
+from config import config
+from src.utils import create_database, user_request, get_and_save_company_data, get_and_save_vacancy_data
+
+def main():
+    """
+    Запуск программы
+    """
+    print("Началось получение и сохранение данных...")
+
+    params = config()
+
+    create_database("hh", params)
+    get_and_save_company_data("hh", params)
+    get_and_save_vacancy_data("hh", params)
+    user_request()
+
+    while True:
+        user_choice = input("Хотите сделать еще один запрос?\n").lower()
+        if user_choice == "да":
+            user_request()
+        else:
+            break
+
+
+if __name__ == '__main__':
+    main()
